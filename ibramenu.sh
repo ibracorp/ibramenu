d4916e0ed0dfed2ed65ea2afe374bf19496d9e95
DiscDuck <github@moll.one>
2022-11-22 15:03:10 +0100

    Init

---
+++ ibramenu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+######################################################################
+# Title   : IBRAMENU
+# By      : DiscDuck
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP™
+######################################################################
