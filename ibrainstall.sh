64fc2dd07d210dee92aa45d154b84feed5637683
DiscDuck <77843475+DiscDuck@users.noreply.github.com>
2022-11-24 03:36:48 -0800

    Update ibrainstall.sh

--- ibrainstall.sh
+++ ibrainstall.sh
@@ -6,3 +6,16 @@
 # License : General Public License GPL-3.0-or-later
 # Another fine product brought to you by IBRACORP™
 ######################################################################
+
+# Check for existing ibramenu folder and clean up if needed
+ifolder="/opt/ibracorp/ibramenu"
+if [ -d $ifolder ]; then
+  rm -r $ifolder
+fi
+mkdir -p $ifolder
+
+# Clone ibramenu
+apt update
+apt install git -y
+git clone -b main --single-branch https://github.com/ibracorp/ibramenu.git $ifolder
+find $ifolder -type f -iname "*.sh" -exec chmod +x {} \;
