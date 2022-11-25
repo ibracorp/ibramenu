ac0eb874a406874f98f3cef8c44e2b6b1e57cc53
DiscDuck <77843475+DiscDuck@users.noreply.github.com>
2022-11-25 00:28:53 -0800

    ibrainit: added ubuntu 22.10 as tested

--- ibrainit.sh
+++ ibrainit.sh
@@ -73,7 +73,7 @@ EOF
 
   distribution="$dist $release"
   case $distribution in
-    "Ubuntu 18.04" | "Ubuntu 20.04" | "Ubuntu 22.04")
+    "Ubuntu 18.04" | "Ubuntu 20.04" | "Ubuntu 22.04" | "Ubuntu 22.10")
       echo "$distribution has been tested and works"
       ;;
     *)
