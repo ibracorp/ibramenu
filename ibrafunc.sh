51898868c9e60649a060653c1ddff73dd672d235
DiscDuck <77843475+DiscDuck@users.noreply.github.com>
2022-11-26 05:31:10 -0800

    ibrafunc: added functions

--- ibrafunc.sh
+++ ibrafunc.sh
@@ -74,3 +74,118 @@ Become a Member and sponsor us: https://ibracorp.io/memberships
 EOF
   tput sgr0
 }
+
+# disclaimer
+disclaimer () {
+  ibralogo
+
+  if [ -f "/opt/ibracorp/IBRADISCLAIMER" ]
+  then
+    if grep -q "IBRADISCLAIMER=accepted" "/opt/ibracorp/IBRADISCLAIMER"
+    then
+      :
+      # Has already been accepted
+    else
+      rm "/opt/ibracorp/IBRADISCLAIMER"
+      exit
+    fi
+  else
+    tee <<-EOF
+Please take a moment and read our IBRACORP Disclaimer:
+https://docs.ibracorp.io/ibracorp/
+EOF
+    read -p "Do you accept our Disclaimer? y/N: " accept
+
+    case "$accept" in
+      [yY])
+        echo "Thank you for accepting the IBRACORP Disclaimer."
+        echo
+        stamp=$(date)
+        tee <<-EOF > /opt/ibracorp/IBRADISCLAIMER
+IBRADISCLAIMER=accepted
+$stamp
+EOF
+          ;;
+      *)
+        echo "IBRACROP Disclaimer has not been accepted. Exiting..."
+        echo
+        exit 0
+          ;;
+    esac
+  fi
+}
+
+environment_check () {
+  # Check for environment files or otherwise create them with defaults
+  if [ ! -d "/opt/appdata" ]
+  then
+    mkdir -p /opt/appdata
+  fi
+  # Timezone
+  if [ ! -f "/opt/appdata/.timezone.env" ]
+  then
+    echo "TZ=UTC" > "/opt/appdata/.timezone.env"
+  fi
+  # PUID and PGID
+  if [ ! -f "/opt/appdata/.id.env" ]
+  then
+    echo "PUID=1000" > "/opt/appdata/.id.env"
+    echo "PGID=1000" >> "/opt/appdata/.id.env"
+  fi
+  # theme.park
+  if [ ! -f "/opt/appdata/.themepark.env" ]
+  then
+    echo "DOCKER_MODS=ghcr.io/gilbn/theme.park:\${TP_APP}" > "/opt/appdata/.themepark.env"
+    echo "TP_THEME=plex" >> "/opt/appdata/.themepark.env"
+  fi
+}
+
+# launch docker compose container
+launchdocker () {
+  target_folder=$1
+  if [ ! -d $target_folder ]; then
+    mkdir -p "$target_folder"
+  fi
+  cp -R * "$target_folder"
+  docker compose -f "$target_folder/compose.yaml" up -d
+}
+
+# display README.md
+readme () {
+  if [ -f "README.md" ]
+  then
+    mdless -P "README.md"
+    read -p "Do you want to continue (y)? " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      return
+    else
+      return 1
+    fi
+  fi
+}
+
+# IBRACORP motd
+ibramotd () {
+  chmod -x /etc/update-motd.d/*
+  tee <<-'EOF' > /etc/update-motd.d/01-ibracorp
+#!/bin/bash
+######################################################################
+# Title   : IBRACORP MOTD
+# By      : DiscDuck
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP   ^d
+######################################################################
+tee <<-EOF
+  ___ ____  ____      _    ____ ___  ____  ____     (tm)
+ |_ _| __ )|  _ \    / \  / ___/ _ \|  _ \|  _ \  
+  | ||  _ \| |_) |  / _ \| |  | | | | |_) | |_) | 
+  | || |_) |  _ <  / ___ \ |__| |_| |  _ <|  __/  
+ |___|____/|_| \_\/_/   \_\____\___/|_| \_\_|     
+$(lsb_release -sd) | CPU Threads: $(lscpu | grep "CPU(s):" | tail +1 | head -1 | awk  '{print $2}') | IP: $(hostname -I | awk '{print $1}') | RAM: $(free -m | grep Mem | awk 'NR=1 {print $2}') MB
+Become a Member and sponsor us: https://ibracorp.io/memberships
+Type 'ibramenu' to launch IBRAMENU.
+EOF
+  echo "EOF" >> /etc/update-motd.d/01-ibracorp
+  chmod +x /etc/update-motd.d/01-ibracorp
+}
