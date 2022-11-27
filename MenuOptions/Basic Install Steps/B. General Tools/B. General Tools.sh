9c80d3737472c79ecbe30e032f6bc150fe3286d8
DiscDuck <77843475+DiscDuck@users.noreply.github.com>
2022-11-27 08:40:27 -0800

    Create B. General Tools.sh

---
+++ MenuOptions/Basic Install Steps/B. General Tools/B. General Tools.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+######################################################################
+# Title   : Install General Tools
+# By      : DiscDuck
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP™
+######################################################################
+
+install_tools () {
+   # Update catalog
+   apt update
+   # Unzip
+   apt install unzip -y
+   # Python 3 (pip)
+   apt install python3-pip -y
+   # Pip psutil
+   apt-get install gcc python3-dev
+   pip install --no-binary :all: psutil
+   # Google Download
+   pip3 install gdown
+   # dos2unix to fix CRLF
+   apt install dos2unix -y
+   # glances monitoring tool
+   apt install glances -y
+   # tmux
+   apt install tmux -y
+   # zsh
+   apt install zsh -y
+}
+
+install_tools
