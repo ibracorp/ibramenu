b48a8cb5f75fa3f685b44da817cb15115cc5deed
DiscDuck <77843475+DiscDuck@users.noreply.github.com>
2022-11-27 08:27:39 -0800

    Create Show Video Adapters.sh

---
+++ MenuOptions/Submenu Basic Functions/Show Video Adapters/Show Video Adapters.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+######################################################################
+# Title   : Show Video Adapters
+# By      : DiscDuck
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP™
+######################################################################
+
+check_display () {
+   sudo lshw -c display
+}
+
+check_display
