dcc74bd4a035daf7424cdc8a1f59a3177cc8adf8
DiscDuck <77843475+DiscDuck@users.noreply.github.com>
2022-11-27 08:39:13 -0800

    Create A. Update and Upgrade.sh

---
+++ MenuOptions/Basic Install Steps/A. Update and Upgrade/A. Update and Upgrade.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+######################################################################
+# Title   : Update and Upgrade
+# By      : DiscDuck
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP™
+######################################################################
+
+# Include ibrafunc for all the awesome functions
+source /opt/ibracorp/ibramenu/ibrafunc.sh
+
+update_upgrade () {
+   sudo apt update
+   sudo apt upgrade -y
+   msgbox "Please remember to reboot if the process did major Upgrades like the Kernel"
+}
+
+update_upgrade
