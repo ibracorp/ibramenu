83d0270ce9f141db9480c5995662ca86d2873d82
DiscDuck <77843475+DiscDuck@users.noreply.github.com>
2022-11-27 04:23:16 -0800

    Create Power down system.sh

---
+++ MenuOptions/Submenu Basic Functions/Power down system/Power down system.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+######################################################################
+# Title   : Power Down
+# By      : DiscDuck
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP™
+######################################################################
+
+# Include ibrafunc for all the awesome functions
+source /opt/ibracorp/ibramenu/ibrafunc.sh
+
+ibralogo
+msgbox "System Power Down"
+read -p "Are you sure? " -n 1 -r
+echo    
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    /sbin/shutdown -h now
+fi
