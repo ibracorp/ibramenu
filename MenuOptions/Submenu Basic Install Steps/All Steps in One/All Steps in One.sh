c1ce999660e38475476405b3968325e082b1f9b9
DiscDuck <77843475+DiscDuck@users.noreply.github.com>
2022-11-28 02:32:34 -0800

    ibramenu: add all steps message boxes

--- MenuOptions/Submenu Basic Install Steps/All Steps in One/All Steps in One.sh
+++ MenuOptions/Submenu Basic Install Steps/All Steps in One/All Steps in One.sh
@@ -11,9 +11,13 @@
 source /opt/ibracorp/ibramenu/ibrafunc.sh
 
 all_steps () {
+  msgbox "Update and Upgrade - Step 1 of 4"
   "../Update and Upgrade/Update and Upgrade.sh"
+  msgbox "General Tools - Step 2 of 4"
   "../General Tools/General Tools.sh"
+  msgbox "Docker Install - Step 3 of 4"
   "../Install Docker/Install Docker.sh"
+  msgbox "Docker Compose Install - Step 4 of 4"
   "../Install Docker Compose/Install Docker Compose.sh"
   msgbox "Please remember to reboot if the process did major Upgrades like the Kernel"
 }
