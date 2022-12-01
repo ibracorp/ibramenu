89111bde3478efd237d9951b5cd5d83004c22c2c
DiscDuck <github@moll.one>
2022-12-01 22:04:30 +0100

    ibramenu: add prowlarr and whisparr

---
+++ MenuOptions/Submenu Media Services/Submenu The ARRs/Prowlarr/Prowlarr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+######################################################################
+# Title   : Install Prowlarr
+# By      : DiscDuck
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP™
+######################################################################
+
+# Include ibrafunc for all the awesome functions
+source /opt/ibracorp/ibramenu/ibrafunc.sh
+
+# App Info
+app="prowlarr"                                   # App Name
+title="Prowlarr"                                 # Readable App Title
+image="lscr.io/linuxserver/prowlarr:develop"     # Image and Tag
+volumes="    volumes:
+      - /opt/appdata/\${APP_NAME:?err}:/config" # Volumes
+tp_app="prowlarr"                                 # Theme Park App Name
+porte="9696"                                    # External Port
+porti="9696"                                    # Internal Port
+extrapayload=""                                 # Extra Payload to add to the Compose
+
+# Execute
+app
---
+++ MenuOptions/Submenu Media Services/Submenu The ARRs/Whisparr/Whisparr.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+######################################################################
+# Title   : Install Whisparr
+# By      : DiscDuck
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP™
+######################################################################
+
+# Include ibrafunc for all the awesome functions
+source /opt/ibracorp/ibramenu/ibrafunc.sh
+
+# App Info
+app="whiasparr"                                # App Name
+title="Whisparr"                               # Readable App Title
+image="cr.hotio.dev/hotio/whisparr:nightly"    # Image and Tag
+volumes="    volumes:
+      - /opt/appdata/\${APP_NAME:?err}:/config
+      - /mnt/media:/media"                     # Volumes
+tp_app=""                                      # Theme Park App Name
+porte="6969"                                   # External Port
+porti="6969"                                   # Internal Port
+extrapayload=""                                # Extra Payload to add to the Compose
+
+# Execute
+app
