187b5a76af6860a07d5c3dba6682ef2a022befc9
DiscDuck <77843475+DiscDuck@users.noreply.github.com>
2022-12-04 11:51:17 -0800

    ibrafunc: introduce ibranet

--- MenuOptions/Submenu Basic Install Steps/Install Docker/Install Docker.sh
+++ MenuOptions/Submenu Basic Install Steps/Install Docker/Install Docker.sh
@@ -8,16 +8,21 @@
 ######################################################################
 
 install_docker () {
-   sudo apt-get remove docker docker-engine docker.io containerd runc
-   sudo apt-get update
-   sudo apt-get install ca-certificates curl gnupg lsb-release
-   sudo rm /usr/share/keyrings/docker-archive-keyring.gpg
-   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-   sudo apt-get update
-   sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-   sudo groupadd docker
-   sudo usermod -aG docker $USER
+  sudo apt-get remove docker docker-engine docker.io containerd runc
+  sudo apt-get update
+  sudo apt-get install ca-certificates curl gnupg lsb-release
+  sudo rm /usr/share/keyrings/docker-archive-keyring.gpg
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+}
+
+create_docker_network () {
+  docker network create ibranet > /dev/null 2>&1
 }
 
 install_docker
+create_docker_network
