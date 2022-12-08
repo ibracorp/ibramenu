d6f6b83a18b43726281d96f2cf60b516882b6160
DiscDuck <github@moll.one>
2022-12-08 22:59:59 +0100

    ibramenu: add uptime kuma

---
+++ MenuOptions/Submenu Networking/Uptime Kuma/Uptime Kuma.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+######################################################################
+# Title   : Install Uptime Kuma
+# By      : DiscDuck
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP™
+######################################################################
+
+# Include ibrafunc for all the awesome functions
+source /opt/ibracorp/ibramenu/ibrafunc.sh
+
+# App Info
+app="uptime-kuma"                     # App Name
+title="Uptime Kuma"                   # Readable App Title
+image="louislam/uptime-kuma:1"        # Image and Tag
+volumes="    volumes:
+      - ./uptime-kuma-data:/app/data" # Volumes
+porte="3001"                          # External Port
+porti="3001"                          # Internal Port
+extrapayload=""                       # Extra Payload to add to the Compose
+
+# Execute
+app
