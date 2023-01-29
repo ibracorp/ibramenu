5d12f16b841da9054746f889b6411ce907e8db17
Angel De La Cruz <angelfranciscoprivado@gmail.com>
2023-01-29 17:21:39 -0500

    removed deoendcy on dockerproxy

--- MenuOptions/Submenu Networking/Traefik/Traefik.sh
+++ MenuOptions/Submenu Networking/Traefik/Traefik.sh
@@ -43,9 +43,7 @@ extrapayload="    ports:
     environment:
       - CF_DNS_API_TOKEN=$CF_API_TOKEN
       - CF_API_EMAIL=${YOUREMAIL}
-    depends_on:
-      - Dockerproxy
-
+    
     "                                 # Extra Payload to add to the Compose add 4 spance to the top group for example environment: and 6 for the childs (if copy and pasting you just need to add the space to the parent).
 
 
