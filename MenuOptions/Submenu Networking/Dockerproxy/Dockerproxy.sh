b1a7cb27590004e06c2dd263e311cd7fdcf46e31
Angel De La Cruz <angelfranciscoprivado@gmail.com>
2023-01-28 22:13:51 -0500

    added docker socket proxy

---
+++ MenuOptions/Submenu Networking/Dockerproxy/Dockerproxy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+######################################################################
+# Title   : Install Komga
+# By      : DiscDuck
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP™
+######################################################################
+# Include ibrafunc for all the awesome functions
+source /opt/ibracorp/ibramenu/ibrafunc.sh
+
+# App Info
+app="Dockerproxy"                                  # App Name
+title="Dockerproxy"                                # Readable App Title
+image="tecnativa/docker-socket-proxy:edge"     # Image and Tag
+volumes="    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock:ro" # Volumes
+tp_app=""                               # Theme Park App Name
+# porte="8085"                                    # External Port
+# porti="8080"                                    # Internal Port
+extrapayload="    environment:
+      CONTAINERS: 1
+      POST: 0
+    privileged: true
+
+    "                                 # Extra Payload to add to the Compose add 4 spance to the top group for example environment: and 6 for the childs (if copy and pasting you just need to add the space to the parent).
+
+# Execute
+app
