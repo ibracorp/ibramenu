23d8b2c5aa6083d57b4b83533b9f1a60f0d697d4
Angel De La Cruz <angelfranciscoprivado@gmail.com>
2023-01-29 17:35:59 -0500

    added traeik2 to the update script

--- ibraupdate.sh
+++ ibraupdate.sh
@@ -8,7 +8,7 @@
 ######################################################################
 
 sudo bash -c "rm -R /opt/ibracorp/ibramenu/"
-sudo bash -c "git clone -b main --single-branch https://github.com/taos15/ibramenu.git /opt/ibracorp/ibramenu"
+sudo bash -c "git clone -b traefik2 --single-branch https://github.com/taos15/ibramenu.git /opt/ibracorp/ibramenu"
 cd find $ifolder -type f -iname "*.sh" -exec chmod +x {} \;
 
 # Include ibrafunc for all the awesome functions
