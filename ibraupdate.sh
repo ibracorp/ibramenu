b105516bb4384dd46413119ed2db8ff70278012d
Angel De La Cruz <angelfranciscoprivado@gmail.com>
2023-02-11 20:55:31 -0500

    added promt to update the custom docker netwrok in .profile

--- ibraupdate.sh
+++ ibraupdate.sh
@@ -11,5 +11,13 @@ sudo bash -c "rm -R /opt/ibracorp/ibramenu/"
 sudo bash -c "git clone -b dev --single-branch https://github.com/taos15/ibramenu.git /opt/ibracorp/ibramenu"
 sudo find $ifolder -type f -iname "*.sh" -exec chmod +x {} \;
 
+# update the custom docker netwrok use in all the containers
+update_docker_network () {
+  read -p "Enter the name of your custome docker network (ex. ibranet) : " customnetwork
+  echo "dockernet=$customnetwork" >> /opt/ibracorp/ibramenu/.profile
+  docker network create $customnetwork > /dev/null 2>&1
+}
+
+update_docker_network
 # Include ibrafunc for all the awesome functions
 source /opt/ibracorp/ibramenu/ibrafunc.sh
