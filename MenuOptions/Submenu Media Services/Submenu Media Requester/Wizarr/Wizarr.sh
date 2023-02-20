df8804446641d72e90462d424606a1ab1b79001c
Angel De La Cruz <angelfranciscoprivado@gmail.com>
2023-02-20 18:21:28 -0500

    changed domain variable name

--- MenuOptions/Submenu Media Services/Submenu Media Requester/Wizarr/Wizarr.sh
+++ MenuOptions/Submenu Media Services/Submenu Media Requester/Wizarr/Wizarr.sh
@@ -18,12 +18,12 @@ volumes="    volumes:
 porte="5690"                                    # External Port
 porti="5690"                                    # Internal Port
 extrapayload="    environment:
-      - APP_URL=https://wizarr.\${YOURDOMAIN}
+      - APP_URL=https://wizarr.\${DOMAIN}
       - DISABLE_BUILTIN_AUTH=false
 #     labels:
 #       - traefik.enable=true
 #       - traefik.http.routers.wizarr.entrypoints=https
-#       - traefik.http.routers.wizarr.rule=Host(\`wizarr.\${YOURDOMAIN}\`)"
+#       - traefik.http.routers.wizarr.rule=Host(\`wizarr.\${DOMAIN}\`)"
 
 
 # Execute
