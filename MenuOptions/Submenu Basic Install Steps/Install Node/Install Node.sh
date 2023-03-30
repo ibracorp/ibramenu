c018da772b5bec2079145c75fbaee1e0d7642d27
Angel De La Cruz <122130728+taos15@users.noreply.github.com>
2023-03-30 16:19:35 -0400

    Main (#6)

---
+++ MenuOptions/Submenu Basic Install Steps/Install Node/Install Node.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+######################################################################
+# Title   : Install Docker Compose
+# By      : Taos15
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP™
+######################################################################
+
+install_node() {
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+  nvm install node
+}
+
+install_node
