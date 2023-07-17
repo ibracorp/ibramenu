17b8ffc333dbc49c702863bf24ce05bb64585505
Angel De La Cruz <angelfranciscoprivado@gmail.com>
2023-07-17 17:33:26 -0400

    feat: add FreshRSS

---
+++ MenuOptions/Submenu Security/Clamav/Clamav.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+######################################################################
+# Title   : Install Komga
+# By      : DiscDuck, Taos15
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP™
+######################################################################
+
+# Include ibrafunc for all the awesome functions
+source /opt/ibracorp/ibramenu/ibrafunc.sh
+
+# App Info
+app="Clamav"                                  # App Name
+title="Clamav"                                # Readable App Title
+image="clamav/clamav"     # Image and Tag
+volumes="    volumes:
+      - /opt/appdata/\${APP_NAME:?err}:/config" # Volumes
+tp_app="Clamav"                               # Theme Park App Name
+porte="3310"                                    # External Port
+porti="3310"                                    # Internal Port
+extrapayload=""                                 # Extra Payload to add to the Compose
+
+
+# Execute
+app
---
+++ MenuOptions/Submenu Tools/FreshRSS/FreshRSS.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+######################################################################
+# Title   : Install Komga
+# By      : DiscDuck, Taos15
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP™
+######################################################################
+
+# Include ibrafunc for all the awesome functions
+source /opt/ibracorp/ibramenu/ibrafunc.sh
+
+# App Info
+app="FreshRSS"                              # App Name
+title="FreshRSS"                            # Readable App Title
+image="lscr.io/linuxserver/freshrss:latest" # Image and Tag
+volumes="    volumes:
+      - /opt/appdata/\${APP_NAME:?err}:/config" # Volumes
+tp_app="Plex"                                   # Theme Park App Name
+porte="8040"                                    # External Port
+porti="80"                                      # Internal Port
+extrapayload=""                                 # Extra Payload to add to the Compose
+
+# Execute
+app
